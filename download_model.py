cc0d51a74b33de5585fbc625cd92e13972a86113
xiongrengrong <xiongrengrong@kuaishou.com>
2026-01-07 12:55:14 +0800

    6

--- download_model.py
+++ download_model.py
@@ -4,7 +4,7 @@ from huggingface_hub import snapshot_download
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 # 替换成你的 HF Token，以 hf_ 开头
-MY_TOKEN = "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" 
+MY_TOKEN = "" 
 
 print("开始下载模型...")
 snapshot_download(
